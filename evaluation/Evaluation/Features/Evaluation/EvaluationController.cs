3b49138659775bcae970b053c60a751dc4e3d3c3
Ousmane <ousmane.thialala@gmail.com>
2020-01-29 22:38:13 +0100

    Evaluation project setup

---
+++ evaluation/Evaluation/Features/Evaluation/EvaluationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Evaluation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Evaluation.Features.Evaluation
+{
+    public class EvaluationController : Controller
+    {
+        private readonly ILogger<EvaluationController> _logger;
+
+        public EvaluationController(ILogger<EvaluationController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
