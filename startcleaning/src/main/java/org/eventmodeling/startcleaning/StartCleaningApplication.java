89a0f2a0fec6d1a54d9cca55092a71b46a9c00ad
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-01-30 11:11:41 +0100

    [start-cleaning] springboot

---
+++ startcleaning/src/main/java/org/eventmodeling/startcleaning/StartCleaningApplication.java
@@ -0,0 +1,13 @@
+package org.eventmodeling.startcleaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartCleaningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartCleaningApplication.class, args);
+	}
+
+}
---
+++ startcleaning/src/test/java/org/eventmodeling/startcleaning/StartCleaningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eventmodeling.startcleaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartCleaningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
