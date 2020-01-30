d409e15e5d209732d58b32aa41d7ee54f7d5a496
Ousmane <ousmane.thialala@gmail.com>
2020-01-30 12:21:06 +0100

    Updated tests to get documentation

--- evaluation/Evaluation.Tests/PublishEvaluationTests.cs
+++ evaluation/Evaluation.Tests/PublishEvaluationTests.cs
@@ -36,84 +36,72 @@ namespace Evaluation.Tests
 			var expectedEvent = new EvaluationPublished(command.GuestId, command.RoomNumber, command.From, command.To, command.Comment, command.Ratings);
 			inMemoryRepository.GetEvents().Should().BeEquivalentTo(new[] { expectedEvent });
 		}
-
-
 	}
 
-	public class EvaluationSpecification : Specification<EvaluationPublished, Evaluate>
-	{
-		private readonly ITestOutputHelper _output;
-		public IEvaluationRepository EvaluationRepository { get; set; } = new InMemoryEvaluationRepository();
+    public class EvaluationSpecification : Specification<EvaluationPublished, Evaluate>
+    {
+        private readonly ITestOutputHelper _output;
+        public IEvaluationRepository EvaluationRepository { get; set; } = new InMemoryEvaluationRepository();
 
-		public EvaluationSpecification(ITestOutputHelper output)
-		{
-			_output = output;
-		}
+        public EvaluationSpecification(ITestOutputHelper output)
+        {
+            _output = output;
+        }
 
-		public override EvaluationPublished Given()
-		{
-			return null;
-		}
+        public override EvaluationPublished Given()
+        {
+            return null;
+        }
 
-		public override Evaluate When()
-		{
-			return new Evaluate();
-		}
+        public override Evaluate When()
+        {
+            return new Evaluate
+            {
+                Comment = "Great Hotel",
+                From = new DateTime(2020, 1, 29),
+                To = new DateTime(2020, 2, 5),
+                GuestId = Guid.Empty,
+                RoomNumber = 1,
+                Ratings = new[]
+                {
+                    new Rating {RateType = "Services", Rate = 4},
+                    new Rating {RateType = "Cleaness", Rate = 4},
+                    new Rating {RateType = "Food", Rate = 4},
+                }
+            };
+        }
 
-		[Fact]
-		public void Publish_an_evaluation_create_an_review_published_event()
-		{
-			IHandle<Evaluate> handler = new EvaluateHandler(EvaluationRepository);
-			Setup(handler);
-			ProducedEvent = EvaluationRepository.GetEvents().ToList();
+        [Fact]
+        public void Publish_an_evaluation_create_an_review_published_event()
+        {
+            IHandle<Evaluate> handler = new EvaluateHandler(EvaluationRepository);
+            Setup(handler);
+            ProducedEvent = EvaluationRepository.GetEvents().ToList();
 
-			Evaluate command = When();
-			var expectedEvent = new EvaluationPublished(
-				command.GuestId,
-				command.RoomNumber,
-				command.From,
-				command.To,
-				command.Comment,
-				command.Ratings);
+            Evaluate command = When();
+            var expectedEvent = new EvaluationPublished(
+                command.GuestId,
+                command.RoomNumber,
+                command.From,
+                command.To,
+                command.Comment,
+                command.Ratings);
 
-			ProducedEvent.Should().BeEquivalentTo(expectedEvent);
-		}
+            ProducedEvent.Should().BeEquivalentTo(expectedEvent);
+        }
 
-		[Fact]
-		public void PrintDocumentation()
-		{
-			_output.WriteLine($"Given: {Given()}");
-			_output.WriteLine($"When: {When()}");
-			Publish_an_evaluation_create_an_review_published_event();
+        [Fact]
+        public void PrintDocumentation()
+        {
+            _output.WriteLine($"Given: {Given()}");
+            _output.WriteLine($"When: {When()}");
+            Publish_an_evaluation_create_an_review_published_event();
 
-			_output.WriteLine("Expected:");
-			foreach (IEvent @event in ProducedEvent)
-			{
-				_output.WriteLine($"\t{@event}");
-			}
-		}
-
-		//public void DocumentSpecification(Specification s)
-		//{
-		////printf(for each given …)
-		////printf when command
-		////	Produced foreach printf
-
-		////	Given a series of events
-		////When a command
-		////	Then a series of event or exception
-		//}
-	}
-
-	//public void DocumentSpecification(Specification s)
-	//{
-	////printf(for each given …)
-	////printf when command
-	////	Produced foreach printf
-
-	////	Given a series of events
-	////When a command
-	////	Then a series of event or exception
-	//}
-
+            _output.WriteLine("Expected:");
+            foreach (IEvent @event in ProducedEvent)
+            {
+                _output.WriteLine($"\t{@event}");
+            }
+        }
+    }
 }
