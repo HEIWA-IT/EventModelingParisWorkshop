76dad95b0f6060afd83bb181a8f6c9f8f18ad870
Jeremy LAIR <Jeremy.LAIR@vinci-energies.net>
2020-01-31 15:58:57 +0100

    POC with subscription

--- evaluation/ConsoleApp1/Program.cs
+++ evaluation/ConsoleApp1/Program.cs
@@ -11,37 +11,56 @@ namespace ConsoleApp1
 	{
 		static void Main()
 		{
+			string streamName = $"{nameof(Evaluation)}_v2";
+
 			Task.Run(() =>
 			{
 				using (IEventStoreConnection session = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113)))
 				{
 					session.ConnectAsync().Wait();
 
+					// SUB
+					EventStoreSubscription eventStoreSubscription = session.SubscribeToStreamAsync(streamName, false, EventAppeared).Result;
+
+					// READ
+					var streamEvents = session.ReadStreamEventsForwardAsync(streamName, 0, 100, false).Result;
+					foreach (ResolvedEvent resolvedEvent in streamEvents.Events)
+					{
+						Print(resolvedEvent, "Read");
+					}
+
+					Console.WriteLine();
+
 					// WRITE
 					var message = new Evaluation("Super", 12);
 					string serializeObject = JsonConvert.SerializeObject(message);
 					byte[] bytes = Encoding.UTF8.GetBytes(serializeObject);
-					var eventData = new EventData(Guid.NewGuid(), $"{nameof(Evaluation)}_v2", true, bytes, null);
-					WriteResult result = session.AppendToStreamAsync($"{nameof(Evaluation)}_v2", ExpectedVersion.Any, eventData).Result;
+					var eventData = new EventData(Guid.NewGuid(), streamName, true, bytes, null);
+					WriteResult result = session.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventData).Result;
 					Console.WriteLine("Write event with data: {0}, metadata: {1}",
 						Encoding.UTF8.GetString(eventData.Data),
 						Encoding.UTF8.GetString(eventData.Metadata));
-
-					// READ
-					var streamEvents = session.ReadStreamEventsForwardAsync($"{nameof(Evaluation)}_v2", 0, 100, false).Result;
-					foreach (ResolvedEvent resolvedEvent in streamEvents.Events)
-					{
-						Console.WriteLine("[{3}] Read event with data: {0}, metadata: {1}, type: {2}",
-							Encoding.UTF8.GetString(resolvedEvent.Event.Data),
-							Encoding.UTF8.GetString(resolvedEvent.Event.Metadata),
-							resolvedEvent.Event.EventType,
-							resolvedEvent.Event.EventNumber
-							);
-					}
 				}
 			}).Wait(1);
 
 			Console.ReadKey(true);
+		}
+
+		private static Task EventAppeared(EventStoreSubscription arg1, ResolvedEvent arg2)
+		{
+			Print(arg2, "Receive");
+			return Task.FromResult(true);
+		}
+
+		private static void Print(ResolvedEvent resolvedEvent, string prefixe)
+		{
+			Console.WriteLine("[{3}] {4} event with data: {0}, metadata: {1}, type: {2}",
+				Encoding.UTF8.GetString(resolvedEvent.Event.Data),
+				Encoding.UTF8.GetString(resolvedEvent.Event.Metadata),
+				resolvedEvent.Event.EventType,
+				resolvedEvent.Event.EventNumber,
+				prefixe
+			);
 		}
 	}
 
