4778006c71e1ee2ad07d73473c6b84f66cf90d9d
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-01-31 16:47:19 +0100

    [start-cleaning] Beginning tests with the real Event Store

--- startcleaning/src/test/java/org/eventmodeling/startcleaning/StartCleaningApplicationTests.java
+++ startcleaning/src/test/java/org/eventmodeling/startcleaning/StartCleaningApplicationTests.java
@@ -1,15 +1,22 @@
 package org.eventmodeling.startcleaning;
 
+import org.eventmodeling.startcleaning.domain.Room;
+import org.eventmodeling.startcleaning.domain.RoomCleaningStarted;
+import org.eventmodeling.startcleaning.infrastructure.RealEventStore;
+import org.eventmodeling.startcleaning.infrastructure.filesystem.adapter.RoomCleaningStartedAdapter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class StartCleaningApplicationTests {
 
 	@Test
 	void contextLoads() {
-
-
+		RealEventStore realEventStore = new RealEventStore();
+		realEventStore.adapter(RoomCleaningStarted.class, new RoomCleaningStartedAdapter());
+		realEventStore.add(new RoomCleaningStarted(new Room(301), LocalDate.now()));
 	}
 
 }
